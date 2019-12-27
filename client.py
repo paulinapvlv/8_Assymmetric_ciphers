a11fe5394a363abaea36314352ef74143e68db23
Polina Pavlova <paulinapvlv@gmail.com>
2019-12-27 09:27:56 +0300

    Доделана передача данных клиент-сервер, шифрование/дешифрование сообщений

--- client.py
+++ client.py
@@ -5,21 +5,28 @@ import random
 
 HOST = "127.0.0.1"
 PORT = functions.check_port(input("Порт: "))
-msg=input("Введите сообщение")
+msg=input("Введите сообщение: ")
 
 sock = socket.socket()
 sock.connect((HOST, PORT))
-conn, addr = sock.accept()
 
-p,g,a = functions.prime_num(),functions.prime_num(),functions.prime_num()
-A = functions.mod_num(p,g,a)
+p,g = functions.prime_num(),functions.prime_num()
+a = random.randint(1,1000)
+A = functions.mod_num(g,p,a)
 
 print("p {}\ng {}\na {}\nA {} ".format(p,g,a,A))
 
 sock.send(pickle.dumps((p,g,A)))
+print("Переменные p, g, A отправлены")
 
-B=pickle.loads(conn.recv(1024))
-K_client=functions.mod_num(B,p,a)
-sock.send(pickle.dumps(K_client)) 
-   
+B=pickle.loads(sock.recv(1024))
+K_client=functions.mod_num(B,p,a) 
+sock.send(pickle.dumps(K_client))
+print("Клиентское значение K отправлено")
+
+send_message=pickle.loads(sock.recv(1024)) 
+if send_message:
+    cyphered=functions.xor(msg,K_client)
+    sock.send(pickle.dumps(cyphered))
+    print("Зашифрованное сообщение отправлено")
 sock.close()
--- server.py
+++ server.py
@@ -9,17 +9,33 @@ PORT = functions.check_port(input("Порт: "))
 sock = socket.socket()
 sock.bind((HOST, PORT))
 sock.listen(1)
-conn, addr = sock.accept()
+while True:
+    conn, addr = sock.accept()
+    request_msg=bool()
 
-p,g,A=pickle.loads(conn.recv(1024))
-b=functions.prime_num()
-B=functions.mod_num(g,p,b)
-print("p {}\ng {}\nA {}\nb {} \nB {}".format(p,g,A,b,B))
+    p,g,A=pickle.loads(conn.recv(1024))
+    b=functions.prime_num()
+    B=functions.mod_num(g,p,b)
+    print("p {}\ng {}\nA {}\nb {} \nB {}".format(p,g,A,b,B))
 
-sock.send(pickle.dumps(B))
-K_server=functions.mod_num(A,p,b)
+    conn.send(pickle.dumps(B))
+    print("B отправлено".format(B))
+    K_server=functions.mod_num(A,p,b)
 
-K_client=pickle.loads(conn.recv(1024))
-if K_server==K_client:
-    print("Одинаковый общий секрет")
-conn.close()
+    K_client=pickle.loads(conn.recv(1024))
+    print("K серверное {},K клиентское {}".format(K_server,K_client))
+
+    if K_server==K_client:
+        print("Одинаковое K")
+        request_msg=True
+    else:
+        print("Неодинаковое K")
+        request_msg=False
+    conn.send(pickle.dumps(request_msg))
+
+    if request_msg:
+       cyphered=pickle.loads(conn.recv(1024))
+       print("Зашифрованное сообщение (получено с клиента) {}".format(cyphered))
+       decyphered=functions.xor(cyphered,K_server)
+       print("Дешифрованное сообщение {}".format(decyphered))
+    conn.close()
